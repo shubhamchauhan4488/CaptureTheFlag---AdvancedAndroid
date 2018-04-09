68f8cc96307088001042c5aa150658f122702aab
Kane Denzil <kaneon95@gmail.com>
2018-04-08 21:53:40 -0400

    permissions

--- app/src/main/java/com/lambton/capturetheflag/Geofire.java
+++ app/src/main/java/com/lambton/capturetheflag/Geofire.java
@@ -1,8 +1,10 @@
 package com.lambton.capturetheflag;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -56,11 +58,12 @@ public class Geofire extends FragmentActivity implements OnMapReadyCallback {
                     }
                 })
                 .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
                     @Override
                     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
                         Log.d(TAG, "Failed to connect to Google API Client -" + connectionResult.getErrorMessage());
-                        requestPermissions(new String[]) {
+                        requestPermissions(new String[]{
                             Manifest.permission.ACCESS_FINE_LOCATION,
 
                                     Manifest.permission.ACCESS_COARSE_LOCATION
