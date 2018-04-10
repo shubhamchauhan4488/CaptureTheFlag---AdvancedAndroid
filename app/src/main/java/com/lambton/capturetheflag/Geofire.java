2ca8a9f4f08bd11b56322704f928e6755d23cfe5
shubhamChauhan <shubhamchauhan4488@gmail.com>
2018-04-10 18:17:15 -0400

    commit
    Errors resolved

--- app/src/main/java/com/lambton/capturetheflag/Geofire.java
+++ app/src/main/java/com/lambton/capturetheflag/Geofire.java
@@ -1,6 +1,7 @@
 package com.lambton.capturetheflag;
 
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -17,26 +18,25 @@ import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-<<<<<<< HEAD
+//<<<<<<< HEAD
 import com.google.android.gms.maps.model.CircleOptions;
-=======
+//=======
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
->>>>>>> 60d9c9545c408b7950e7c9c2bb11ba3799d9bac1
+//>>>>>>> 60d9c9545c408b7950e7c9c2bb11ba3799d9bac1
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Geofire extends FragmentActivity implements OnMapReadyCallback,
-
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener{
 
     private GoogleMap mMap;
-
     private static final String TAG = MainActivity.class.getSimpleName();
     private Marker geoFenceMarker;
     private static final long GEO_DURATION = 60 * 60 * 1000;
@@ -73,15 +73,8 @@ public class Geofire extends FragmentActivity implements OnMapReadyCallback,
                     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
                         Log.d(TAG, "Failed to connect to Google API Client -" + connectionResult.getErrorMessage());
-<<<<<<< HEAD
                         requestPermissions(new String[]{
-                            Manifest.permission.ACCESS_FINE_LOCATION,
-
-                                    Manifest.permission.ACCESS_COARSE_LOCATION
-=======
-                        requestPermissions(new String[] {
                             Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
->>>>>>> 60d9c9545c408b7950e7c9c2bb11ba3799d9bac1
                         },1234);
 
                     }
@@ -94,11 +87,8 @@ public class Geofire extends FragmentActivity implements OnMapReadyCallback,
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(43.7822457, -79.349838);
-<<<<<<< HEAD
         mMap.addMarker(new MarkerOptions().position(sydney).title("Lambton College"));
         LatLng dangerous_area = new LatLng(37.7533,-122.4056);
         mMap.addCircle(new CircleOptions()
@@ -106,14 +96,12 @@ public class Geofire extends FragmentActivity implements OnMapReadyCallback,
                 .radius(500) //in mts
                 .strokeColor(Color.BLUE)
                 .fillColor(0x220000FF)
-                .strokeWidth(5.0f)
-=======
+                .strokeWidth(5.0f));
 
         geoFenceMarker = mMap.addMarker(new MarkerOptions().position(sydney).title("Lambton College"));
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         beginGeofence();
->>>>>>> 60d9c9545c408b7950e7c9c2bb11ba3799d9bac1
     }
 
     @Override
@@ -170,6 +158,27 @@ public class Geofire extends FragmentActivity implements OnMapReadyCallback,
 
         // Call GoogleApiClient connection when starting the Activity
         googleapiclient.connect();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 
 
